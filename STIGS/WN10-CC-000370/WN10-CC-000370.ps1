d95a69ed07cafa278820257300cf358159209390
Vladimir <89713001+vladdyvlad@users.noreply.github.com>
2025-05-23 23:30:34 -0400

    Create WN10-CC-000370.ps1

---
+++ STIGS/WN10-CC-000370/WN10-CC-000370.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This script disables the use of a PIN for logging into a domain, enforcing more secure login methods through registry modifications.
+
+.NOTES
+    Author          : Vladimir 
+    LinkedIn        : https://www.linkedin.com/in/vlaexis/
+    GitHub          : https://github.com/Vladdyvlad
+    Date Created    : 2025-05-23
+    Last Modified   : 2025-05-23
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000370
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000370.ps1 
+#>
+
+# Define the registry path and values
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System"
+$regName = "AllowDomainPINLogon"
+$regValue = 0
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Apply the registry setting
+New-ItemProperty -Path $regPath -Name $regName -Value $regValue -PropertyType DWord -Force | Out-Null
+
+Write-Host "'Turn on convenience PIN sign-in' policy set to 'Disabled' successfully."
+ 
